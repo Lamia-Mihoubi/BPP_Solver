826f7e4cdf2b30681413922f967cacfa432b0185
Yasmine Bachi <36101876+yasmine9@users.noreply.github.com>
2020-04-30 11:46:59 +0100

    filled the methodes so that they call the functions on the other files

--- BPP_Solver.py
+++ BPP_Solver.py
@@ -1,75 +1,97 @@
-{
- "cells": [
-  {
-   "cell_type": "markdown",
-   "metadata": {},
-   "source": [
-    "# Bin Packing Problem Solver"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "class BPP_Solver:\n",
-    "    # the method for solving the BPP using BB:\n",
-    "    def bb_solver(self, nbrItems, binSize, itemWeights):\n",
-    "        optBinNbr = 0\n",
-    "        itemsDist = dict() # the structure of the dictionary: {itemNumber: binNumberContainingIt, ...}\n",
-    "        # code\n",
-    "        return optBinNbr, itemsDist\n",
-    "    \n",
-    "    # the method for solving the BPP using BFD:\n",
-    "    def BFD_solver(self, nbrItems, binSize, itemWeights):\n",
-    "        optBinNbr = 0\n",
-    "        itemsDist = dict() # the structure of the dictionary: {itemNumber: binNumberContainingIt, ...}\n",
-    "        # code\n",
-    "        return optBinNbr, itemsDist\n",
-    "    \n",
-    "    # the method for solving the BPP using FFD:\n",
-    "    def FFD_solver(self, nbrItems, binSize, itemWeights):\n",
-    "        optBinNbr = 0\n",
-    "        itemsDist = dict() # the structure of the dictionary: {itemNumber: binNumberContainingIt, ...}\n",
-    "        # code\n",
-    "        return optBinNbr, itemsDist\n",
-    "    \n",
-    "    # the method for solving the BPP using WOA:\n",
-    "    def WOA_solver(self, nbrItems, binSize, itemWeights):\n",
-    "        optBinNbr = 0\n",
-    "        itemsDist = dict() # the structure of the dictionary: {itemNumber: binNumberContainingIt, ...}\n",
-    "        # code\n",
-    "        return optBinNbr, itemsDist\n",
-    "    \n",
-    "    # the method for solving the BPP using ILWOA:\n",
-    "    def ILWOA_solver(self, nbrItems, binSize, itemWeights):\n",
-    "        optBinNbr = 0\n",
-    "        itemsDist = dict() # the structure of the dictionary: {itemNumber: binNumberContainingIt, ...}\n",
-    "        # code\n",
-    "        return optBinNbr, itemsDist"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+"""
+BPP_Solver : la classe main du projet , d'ici on peut appeler les differentes méthodes implementées, pour résoudre
+une instance du Bin packing problem
+un exemple d'utilisation est donnée à la fin du fichier
+
+la méthode solve instances permet de résoudre un ensemble d'instances par les méthodes implementées
+il suffit de donner en entrées le chemin vers les fichiers d'instances, ansi que mettre à True le booléen correspodant
+à la/les méthodes qu'on veut executer 
+"""
+import os
+
+from Exact_methods.BB import run_BB
+from Exact_methods.Exhaustive import run_exhaustive
+from Exact_methods.BBA import run_BBA
+from Exact_methods.DP import run_DP
+from Instances_reader import ReadInstance
+
+
+class BPP_Solver:
+    # methode qui résout l'ensemble d'instances qui se trouvent dans le dossier "file path"
+    # en utilisant les methodes qui sont à True dans les paramètres
+    # elle retourne les solutions dans l'ordre des parametres qui sont à True
+    def solve_instances(self, filepath, bb=False, bba=False, dp=False, exh=False):
+
+        solutions = []
+        directory = filepath
+        i = 0
+        for filename in os.listdir(directory):
+
+            problem = BPP_Solver()
+            if filename.endswith(".txt"):
+                n, c, list= ReadInstance(directory + "\\" + filename)
+                list.sort(reverse=True)
+                solutions.append([])
+                if bb: solutions[i].append(BPP_Solver.bb_solver(problem, n, c, list))
+                if bba: solutions[i].append(BPP_Solver.bba_solver(problem, n, c, list))
+                if dp: solutions[i].append(BPP_Solver.dp_solver(problem, n, c, list))
+                if exh: solutions[i].append(BPP_Solver.exhaustive_solver(problem, n, c, list))
+
+                i = i + 1
+        return solutions
+
+    # the method for solving the BPP using BB:
+    def bb_solver(self, nbrItems, binSize, itemWeights):
+        optBinNbr, liste, t_exec = run_BB(nbrItems, binSize, itemWeights)
+        return optBinNbr, liste, t_exec
+
+    # the method for solving the BPP using BBA (amelioarted):
+    def bba_solver(self, nbrItems, binSize, itemWeights):
+        optBinNbr, liste, t_exec = run_BBA(nbrItems, binSize, itemWeights)
+        return optBinNbr, liste, t_exec
+
+    # the method for solving the BPP using DP:
+    def dp_solver(self, nbrItems, binSize, itemWeights):
+        optBinNbr, liste, t_exec = run_DP(nbrItems, binSize, itemWeights)
+        return optBinNbr, liste, t_exec
+
+    # the method for solving the BPP using Exhaustive search:
+    def exhaustive_solver(self, nbrItems, binSize, itemWeights):
+        optBinNbr, liste, t_exec = run_exhaustive(nbrItems, binSize, itemWeights)
+        return optBinNbr, liste, t_exec
+
+    # the method for solving the BPP using BFD:
+    def BFD_solver(self, nbrItems, binSize, itemWeights):
+        optBinNbr = 0
+        itemsDist = dict()  # the structure of the dictionary: {itemNumber: binNumberContainingIt, ...}
+        # code
+        return optBinNbr, itemsDist
+
+    # the method for solving the BPP using FFD:
+    def FFD_solver(self, nbrItems, binSize, itemWeights):
+        optBinNbr = 0
+        itemsDist = dict()  # the structure of the dictionary: {itemNumber: binNumberContainingIt, ...}
+        # code
+        return optBinNbr, itemsDist
+
+    # the method for solving the BPP using WOA:
+    def WOA_solver(self, nbrItems, binSize, itemWeights):
+        optBinNbr = 0
+        itemsDist = dict()  # the structure of the dictionary: {itemNumber: binNumberContainingIt, ...}
+        # code
+        return optBinNbr, itemsDist
+
+    # the method for solving the BPP using ILWOA:
+    def ILWOA_solver(self, nbrItems, binSize, itemWeights):
+        optBinNbr = 0
+        itemsDist = dict()  # the structure of the dictionary: {itemNumber: binNumberContainingIt, ...}
+        # code
+        return optBinNbr, itemsDist
+
+
+# Exemple d'utilisation
+problem = BPP_Solver()
+n = 5
+c = 20
+list = [10, 5, 20, 5, 20]
+print(BPP_Solver.solve_instances(problem, filepath="test", bb=True, bba=False, dp=False, exh=True))
