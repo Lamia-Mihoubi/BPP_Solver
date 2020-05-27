1275c762c9010fedf78a2553e51b2442e8ffc117
Mihoubi Lamia Zohra <gl_mihoubi@esi.dz>
2020-05-27 15:58:52 +0200

    Added WOA report chapter

--- Méta_Heuristiques/WOA_.py
+++ Méta_Heuristiques/WOA_.py
@@ -14,6 +14,7 @@ class WOA:
     def __init__(
         self,
         objects_list,
+        capacity,
         search_agents_nbr=20,
         max_iter=40,
         b=1,
@@ -22,6 +23,7 @@ class WOA:
         discretize=LOV,
     ):
         self.objects = objects_list
+        self.capacity = capacity
         self.search_agents_nbr = search_agents_nbr
         self.max_iter = max_iter
         self.b = b
@@ -53,7 +55,7 @@ class WOA:
 
         return np.array(list(k for k, _ in itertools.groupby(population)))
 
-    def init_heuristic(heuristic):
+    def init_heuristic(self, heuristic):
         pass
 
     def get_bin_nbr(self,sol,capacity):
@@ -67,14 +69,14 @@ class WOA:
             if obj != None:
                 weight_sum += obj.weight
             else:
-                print("obj {0} doesn't exist in list {1} ".format(i, objects))
+                print("obj {0} doesn't exist in list {1} ".format(i, self.objects))
             # if the capacity of the bin is filled
             if capacity < weight_sum:
                 nbr_bins_used += 1
                 if obj != None:
                     weight_sum = obj.weight
                 else:
-                    print("obj {0} doesn't exist in list {1} ".format(i, objects))
+                    print("obj {0} doesn't exist in list {1} ".format(i, self.objects))
             elif i == sol[sol_len - 1]:
                 nbr_bins_used += 1
 
@@ -115,7 +117,7 @@ class WOA:
         eval_sols = [self.eval_func(s, self.objects, capacity) for s in pop]
         leader_sol=min(eval_sols)
         leader_index = eval_sols.index(leader_sol)
-        leaders=[]
+        leaders=[leader_sol]
         for i in range(self.max_iter):
             a = self.a - i * 2 / self.max_iter
             a2 = -1 + i * (-1) / self.max_iter
@@ -168,8 +170,6 @@ class WOA:
 
                 leader_sol=evaluation
             else :
-
-
                 mutation=self.mutation(pop[sol_index])
                 evaluation = self.eval_func(mutation, self.objects, capacity)
                 if (leader_sol > evaluation):
@@ -180,5 +180,82 @@ class WOA:
             eval_sols = [self.eval_func(s, self.objects, capacity) for s in pop]
             leader_sol=min(eval_sols)
             leader_index = eval_sols.index(leader_sol)
+            leaders.append(leader_sol)
+        return pop[leader_index], self.get_bin_nbr(pop[leader_index],capacity), leaders
 
-        return pop[leader_index],self.get_bin_nbr(pop[leader_index],capacity)
+    def optimize_param(self, params):
+        self.search_agents_nbr = params[0] 
+        self.max_iter = params[1]
+        self.b = params[2]
+        self.a = params[3]
+        sim=simulation_chaotic(max_iter=self.max_iter)
+        ps=sim.logistic_map(random.random())
+        pop = self.rand_init_population()
+        eval_sols = [self.eval_func(s, self.objects, self.capacity) for s in pop]
+        leader_sol=min(eval_sols)
+        leader_index = eval_sols.index(leader_sol)
+        leaders=[leader_sol]
+        for i in range(self.max_iter):
+            a = self.a - i * 2 / self.max_iter
+            a2 = -1 + i * (-1) / self.max_iter
+            for sol_index in range(pop.shape[0]):
+                r1 = random.random()
+                r2 = random.random()
+
+                A = 2 * a * r1 - a
+                # ILWOA CHANGE:Utiliser la marche aléatoire de levy Pour le C
+                C = 2 * r2
+
+                l = (a2 - 1) * random.random() + 1
+                #ILWOA CHANGE: utiliser chaotic map to initialize p
+                p=ps[i]
+                #p = random.random()
+                if sol_index != leader_index:
+                    if p < 0.5:
+                        if abs(A) >= 1:
+                            rand_leader_index = math.floor(
+                                pop.shape[0] * random.random()
+                            )
+                            x_rand = pop[rand_leader_index]
+                            D_x_rand = np.absolute(C * x_rand - pop[sol_index])
+                            # I have one issue here, what if the result contains some negative values
+                            # would I still apply LOV directly on the result vector or should
+                            # I constrain the solution or something, they use here lower and upper
+                            # bounds to define when is the resulting array out of our search
+                            # domaine so do we need to define them too here? and how to do it?
+
+                            pop[sol_index] = self.discretize(x_rand - A * D_x_rand)
+                        else:
+                            D_leader = np.absolute(
+                                C * pop[leader_index] - pop[sol_index]
+                            )
+                            pop[sol_index] = self.discretize(
+                                pop[leader_index] - A * D_leader
+                            )
+
+                    else:  # if p >= 0.5
+                        dist_to_leader = np.absolute(pop[leader_index] - pop[sol_index])
+                        pop[sol_index]= self.discretize(
+                            dist_to_leader
+                            * math.exp(self.b * l)
+                            * math.cos(l * 2 * math.pi)
+                            + pop[leader_index]
+                        )
+            evaluation=self.eval_func(pop[sol_index],self.objects,self.capacity)
+
+            if(leader_sol>evaluation):
+
+                leader_sol=evaluation
+            else :
+                mutation=self.mutation(pop[sol_index])
+                evaluation = self.eval_func(mutation, self.objects, self.capacity)
+                if (leader_sol > evaluation):
+                   pop[sol_index]=mutation
+                   leader_sol=evaluation
+
+            pop = np.unique(pop, axis=0)
+            eval_sols = [self.eval_func(s, self.objects, self.capacity) for s in pop]
+            leader_sol=min(eval_sols)
+            leader_index = eval_sols.index(leader_sol)
+            leaders.append(leader_sol)
+        return self.get_bin_nbr(pop[leader_index],self.capacity)
