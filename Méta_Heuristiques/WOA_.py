749500d98427d78e2b80a518f0f03f81d5ecaacc
xxCoder35 <55505722+xxCoder35@users.noreply.github.com>
2020-06-02 11:48:56 +0200

    Add files via upload
    ILAWOA

--- Méta_Heuristiques/WOA_.py
+++ Méta_Heuristiques/WOA_.py
@@ -6,6 +6,7 @@ from decimal import *
 import itertools
 import Chaoticmap as ch
 from Chaoticmap import simulation_chaotic
+from Méthodes_Heuristiques import Functions as fct
 
 import Instances_generator as gen
 from functions import occupency, LOV
@@ -15,20 +16,21 @@ class WOA:
     def __init__(
         self,
         objects_list,
-        capacity,
-        search_agents_nbr=50,
-        max_iter=50,
-        b=1.5,
-        a=2,
+        search_agents_nbr=10,
+        max_iter=30,
+        beta=0.34,#0.33<beta<2
+        b=7.36,
+        a=20,
+
         eval_func=occupency,
         discretize=LOV,
     ):
         self.objects = objects_list
-        self.capacity = capacity
         self.search_agents_nbr = search_agents_nbr
         self.max_iter = max_iter
         self.b = b
         self.a = a
+        self.beta=beta
         self.eval_func = eval_func
         self.discretize = LOV
 
@@ -49,9 +51,9 @@ class WOA:
 
     def rand_init_population(self,capacity):
         population = []
-        # add=self.init_heuristic(capacity)
-        # print(self.get_bin_nbr(add,capacity))
-        # population.append(add)
+        add=self.init_heuristic( capacity)
+
+        population.append(add)
         for i in range(self.search_agents_nbr):
             population.append(self.rand_init_sol())
         # removing duplicate solutions:
@@ -73,7 +75,7 @@ class WOA:
 
             # Find the first bin that can accommodate
             # weight[i]
-            j = 0
+            j = 0;
 
             # Initialize minimum space left and index
             # of best bin
@@ -91,7 +93,6 @@ class WOA:
             if (min == c + 1):
                 bin_rem[res] = c -self.objects[i].weight
                 sol.append([i, res])
-
                 res += 1
             else:  # Assign the item to best bin
                 bin_rem[bi] -= self.objects[i].weight
@@ -107,25 +108,35 @@ class WOA:
         return list(unzipped_list[0])
 
 
-    def get_bin_nbr(self,sol,capacity):
+    def get_bin_nbr(self,sol,capacity,opt):
         weight_sum = 0.0
         nbr_bins_used = 0
         # calculating the occupency of every bin
         sol_len=len(sol)
+        rangement=[]
+        bin=[]
+        i=0
         for i in sol:
             # getting the object of indice i
             obj=self.objects[i]
             if obj != None:
                 weight_sum += obj.weight
+                bin.append(obj.weight)
+
             else:
-                print("obj {0} doesn't exist in list {1} ".format(i, self.objects))
+                print("obj {0} doesn't exist in list {1} ".format(i, objects))
             # if the capacity of the bin is filled
             if capacity < weight_sum:
                 nbr_bins_used += 1
                 if obj != None:
+                    bin=bin[:-1]
+                    rangement.append(bin)
+                    bin=[]
+                    bin.append(obj.weight)
+
                     weight_sum = obj.weight
                 else:
-                    print("obj {0} doesn't exist in list {1} ".format(i, self.objects))
+                    print("obj {0} doesn't exist in list {1} ".format(i, objects))
             elif i == sol[sol_len - 1]:
                 nbr_bins_used += 1
 
@@ -166,7 +177,6 @@ class WOA:
                     a2 = sol[move_pos:len(sol)]
                 else:
                     a2 = sol[init_sub + length_sub:len(sol)]
-
             a1 = np.append(a1, tomove)
             a1 = np.append(a1, a2)
             if (init_sub > move_pos):
@@ -179,16 +189,14 @@ class WOA:
 
         return sol.astype(int)
 
-    def optimize(self, capacity,beta=1.5):
-        sim=simulation_chaotic(max_iter=self.max_iter)
-        ps=sim.logistic_map(0.9)
-        cs=sim.levy(self.max_iter,beta)
+    def optimize(self, capacity,beta):
+        sim=simulation_chaotic(max_iter=self.max_iter,biotic_potential=3.9259904913432475)
+        ps=sim.logistic_map(0.63)
         pop = self.rand_init_population(capacity)
-
         eval_sols = [self.eval_func(s, self.objects, capacity) for s in pop]
         leader_sol=min(eval_sols)
         leader_index = eval_sols.index(leader_sol)
-        leaders=[leader_sol]
+        leaders=[]
         for i in range(self.max_iter):
             a = self.a - i * 2 / self.max_iter
             a2 = -1 + i * (-1) / self.max_iter
@@ -198,19 +206,22 @@ class WOA:
 
                 A = 2 * a * r1 - a
                 # ILWOA CHANGE:Utiliser la marche aléatoire de levy Pour le C
-                C = cs[i]
+
 
                 l = (a2 - 1) * random.random() + 1
                 #ILWOA CHANGE: utiliser chaotic map to initialize p
                 p=ps[i]
                 #p = random.random()
                 if sol_index != leader_index:
+
                     if p < 0.5:
                         if abs(A) >= 1:
                             rand_leader_index = math.floor(
                                 pop.shape[0] * random.random()
                             )
                             x_rand = pop[rand_leader_index]
+                            C = sim.levy(np.absolute(
+                            x_rand- pop[sol_index]),pop[sol_index] , len(pop[sol_index]), self.beta)
                             D_x_rand = np.absolute(C * x_rand - pop[sol_index])
                             # I have one issue here, what if the result contains some negative values
                             # would I still apply LOV directly on the result vector or should
@@ -220,6 +231,7 @@ class WOA:
 
                             pop[sol_index] = self.discretize(x_rand - A * D_x_rand)
                         else:
+                            C = sim.levy(np.absolute(pop[leader_index] - pop[sol_index]),pop[sol_index] , len(pop[sol_index]), self.beta)
                             D_leader = np.absolute(
                                 C * pop[leader_index] - pop[sol_index]
                             )
@@ -236,17 +248,16 @@ class WOA:
                             + pop[leader_index]
                         )
             evaluation=self.eval_func(pop[sol_index],self.objects,capacity)
-            print(self.get_bin_nbr(pop[sol_index], capacity))
+
 
             if(leader_sol>evaluation):
-                print("mut")
+                print("mut1")
                 leader_sol=evaluation
             else :
                 mutation=self.mutation(pop[sol_index])
-
                 evaluation = self.eval_func(mutation, self.objects, capacity)
                 if (leader_sol > evaluation):
-                   print("mut")
+                   print("mut2")
                    pop[sol_index]=mutation
                    leader_sol=evaluation
 
@@ -254,82 +265,5 @@ class WOA:
             eval_sols = [self.eval_func(s, self.objects, capacity) for s in pop]
             leader_sol=min(eval_sols)
             leader_index = eval_sols.index(leader_sol)
-            leaders.append(leader_sol)
-        return pop[leader_index], self.get_bin_nbr(pop[leader_index],capacity), leaders
-
-    def optimize_param(self, nb_whales, max_iter, b, a):
-        self.search_agents_nbr = nb_whales 
-        self.max_iter = max_iter
-        self.b = b
-        self.a = a
-        sim=simulation_chaotic(max_iter=self.max_iter)
-        ps=sim.logistic_map(random.random())
-        pop = self.rand_init_population(self.capacity)
-        eval_sols = [self.eval_func(s, self.objects, self.capacity) for s in pop]
-        leader_sol=min(eval_sols)
-        leader_index = eval_sols.index(leader_sol)
-        leaders=[leader_sol]
-        for i in range(self.max_iter):
-            a = self.a - i * 2 / self.max_iter
-            a2 = -1 + i * (-1) / self.max_iter
-            for sol_index in range(pop.shape[0]):
-                r1 = random.random()
-                r2 = random.random()
-
-                A = 2 * a * r1 - a
-                # ILWOA CHANGE:Utiliser la marche aléatoire de levy Pour le C
-                C = 2 * r2
-
-                l = (a2 - 1) * random.random() + 1
-                #ILWOA CHANGE: utiliser chaotic map to initialize p
-                p=ps[i]
-                #p = random.random()
-                if sol_index != leader_index:
-                    if p < 0.5:
-                        if abs(A) >= 1:
-                            rand_leader_index = math.floor(
-                                pop.shape[0] * random.random()
-                            )
-                            x_rand = pop[rand_leader_index]
-                            D_x_rand = np.absolute(C * x_rand - pop[sol_index])
-                            # I have one issue here, what if the result contains some negative values
-                            # would I still apply LOV directly on the result vector or should
-                            # I constrain the solution or something, they use here lower and upper
-                            # bounds to define when is the resulting array out of our search
-                            # domaine so do we need to define them too here? and how to do it?
-
-                            pop[sol_index] = self.discretize(x_rand - A * D_x_rand)
-                        else:
-                            D_leader = np.absolute(
-                                C * pop[leader_index] - pop[sol_index]
-                            )
-                            pop[sol_index] = self.discretize(
-                                pop[leader_index] - A * D_leader
-                            )
-
-                    else:  # if p >= 0.5
-                        dist_to_leader = np.absolute(pop[leader_index] - pop[sol_index])
-                        pop[sol_index]= self.discretize(
-                            dist_to_leader
-                            * math.exp(self.b * l)
-                            * math.cos(l * 2 * math.pi)
-                            + pop[leader_index]
-                        )
-            evaluation=self.eval_func(pop[sol_index],self.objects,self.capacity)
-
-            if(leader_sol>evaluation):
-
-                leader_sol=evaluation
-            else :
-                mutation=self.mutation(pop[sol_index])
-                evaluation = self.eval_func(mutation, self.objects, self.capacity)
-                if (leader_sol > evaluation):
-                   pop[sol_index]=mutation
-                   leader_sol=evaluation
-
-            pop = np.unique(pop, axis=0)
-            eval_sols = [self.eval_func(s, self.objects, self.capacity) for s in pop]
-            leader_sol=min(eval_sols)
-            leader_index = eval_sols.index(leader_sol)
-            leaders.append(leader_sol)
-        return self.get_bin_nbr(pop[leader_index],self.capacity)
+        print(pop[leader_index])
+        return pop[leader_index],self.get_bin_nbr(pop[leader_index],capacity,opt=1)
