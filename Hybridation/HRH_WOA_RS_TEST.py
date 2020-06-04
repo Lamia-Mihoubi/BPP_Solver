4e7a3529aee10bceb8d7bcdfc933519c2282ae1c
yasmine9 <gy_bachi@esi.dz>
2020-06-05 00:03:05 +0100

    Hybridaiton HRH WOA

---
+++ Hybridation/HRH_WOA_RS_TEST.py
@@ -0,0 +1,42 @@
+import Instances_reader
+import time
+import os
+from Méta_Heuristiques.get_opt_sol import get_opt_sol
+from Hybridation import HRH_WOA_RS
+from Méta_Heuristiques.Recuit_Sim import RS
+
+rs = RS()
+
+print("__________CLASSE 01 ________________")
+for filename in os.listdir("C:\\Users\\BACHI\\Desktop\\OPT_project\\Instances_scholl\\classe1"):
+    if filename.endswith(".txt") :
+        n, c, list = Instances_reader.ReadInstance(
+            "C:\\Users\\BACHI\\Desktop\\OPT_project\\Instances_scholl\\classe1" + "\\" + filename)
+        print(filename)
+        opt = get_opt_sol(1, filename)
+        print("\tSolution {}".format(opt))
+        sol = HRH_WOA_RS.hrh_woa_rs(n,c,list)
+
+
+
+print("__________CLASSE 02 ________________")
+
+for filename in os.listdir("C:\\Users\\BACHI\\Desktop\\OPT_project\\Instances_scholl\\classe2"):
+    if filename.endswith(".txt") :
+        n, c, list = Instances_reader.ReadInstance(
+            "C:\\Users\\BACHI\\Desktop\\OPT_project\\Instances_scholl\\classe2" + "\\" + filename)
+        print(filename)
+        opt = get_opt_sol(2, filename)
+        print("\tSolution {}".format(opt))
+        sol = HRH_WOA_RS.hrh_woa_rs(n,c,list)
+
+
+print("__________CLASSE 03 ________________")
+for filename in os.listdir("C:\\Users\\BACHI\\Desktop\\OPT_project\\Instances_scholl\\classe3"):
+    if filename.endswith(".txt") and filename.startswith("H"):
+        n, c, list = Instances_reader.ReadInstance(
+            "C:\\Users\\BACHI\\Desktop\\OPT_project\\Instances_scholl\\classe3" + "\\" + filename)
+        print(filename)
+        opt = get_opt_sol(3, filename)
+        print("\tSolution {}".format(opt))
+        sol = HRH_WOA_RS.hrh_woa_rs(n,c,list)
