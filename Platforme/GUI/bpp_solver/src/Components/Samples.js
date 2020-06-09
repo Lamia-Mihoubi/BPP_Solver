df76985e57eec0aafed985b6bd20b4c7eebd74e9
xxCoder35 <55505722+xxCoder35@users.noreply.github.com>
2020-06-09 17:09:10 +0200

    Add files via upload

--- Platforme/GUI/bpp_solver/src/Components/Samples.js
+++ Platforme/GUI/bpp_solver/src/Components/Samples.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container'
 import FormNC from './FormNC'
+import ChooseMthd from './ChooseMthd';
 
 class MainContent1 extends React.Component{
 
@@ -10,6 +11,7 @@ class MainContent1 extends React.Component{
             <Container >
                 <h1>Générer une instance aléatoire</h1>
                 <FormNC props />
+                <ChooseMthd handleValider={this.handleValider}></ChooseMthd>
             </Container>
             
             );
