c5aaa76ced1c99423eeba3c212d6fe6f7e072451
xxCoder35 <gf_saadi@esi.dz>
2020-06-10 18:12:18 +0100

    Backend

--- Platforme/GUI/bpp_solver/src/Components/FormNC.js
+++ Platforme/GUI/bpp_solver/src/Components/FormNC.js
@@ -12,39 +12,70 @@ import ListItemText from '@material-ui/core/ListItemText';
 import { FixedSizeList } from 'react-window';
 import data from '../data/instance.json'
 class FormNC extends React.Component
-{   const(props)
-    {
-        
-        this.objects=props.objects
+{   constructor(props)
+    {  super(props);
+        this.state = {
+         n:0,
+         c:1,
+         objects:[0,12,23]
+        };  
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange1 = this.handleChange1.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
+        this.renderRow=this.renderRow.bind(this)
     }
-    generateclick(){
-        //send request first
-        console.log("clicked")
-        
-    }
+ 
     renderRow() {
-       return( data.map(d=>(
+       return( this.state.objects.map(d=>(
            <ListItem>{d}</ListItem>
        ) )
        )
     }
+    handleChange1(event)
+     {    this.setState({c: event.target.value}); 
+     }
+     handleChange2(event)
+     {    this.setState({n: event.target.value}); 
+     }
+    handleSubmit(event) {
+       //c'est la win nebe3tou la rqt
+        alert('Le n a été soumis : ' + this.state.n);
+        event.preventDefault();
+        var data = {
+            "n": this.state.n,
+            "c": this.state.c
+         }
+         
+         fetch("https://....", {
+            method: "POST",
+            headers: { 'Accept': 'application/json','Content-Type': 'application/json',},
+            body:  JSON.stringify(data)
+         })
+         .then(function(response){ 
+          return response.json();   
+         })
+         .then(function(data){ 
+         console.log(data)
+         this.setState({objects:data})
+         });
+      }
 render()
 {
     return(
         <React.Fragment>
         
         <Box m={2} p={2}>
-            <form>
+            <form onSubmit={this.handleSubmit} >
                 <Grid container spacing={3}>
                 <Grid item xs={10} sm={3}>
-                    <TextField  variant="outlined"  label="Nombre d'objets " size="small" />
+                    <TextField  variant="outlined"  label="Nombre d'objets " size="small" value={this.state.n} onChange={this.handleChange2}/>
                 </Grid>
                 <Grid item xs={10} sm={3}>
-                    <TextField variant="outlined" label="Capacité des boites " size="small" />
+                    <TextField variant="outlined"  label="Capacité des boites " size="small" value={this.state.c} onChange={this.handleChange1} />
                 </Grid>
                 
                 <Grid item xs={12}>
-                         <Button variant="contained" color="primary"  onClick={this.generateclick}>
+                         <Button variant="contained" color="primary" type="submit" >
                     Générer
                 </Button>
                 </Grid>
